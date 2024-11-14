e51e87acd782ccdc5a4b30b0d3d4906102ad9df5
xkc <xkylechan@gmail.com>
2024-11-14 01:11:28 -0700

    created a 16x16 grid using js.  adds class name row to rows div, and classname col to col divs

---
+++ script.js
@@ -0,0 +1,19 @@
+const getContainer = document.getElementById('container');
+
+function createGrid() {
+  for (let i = 0; i < 16; i++) {
+    const getRow = document.createElement('div');
+    getRow.classList.add('grid', 'row');
+    getContainer.appendChild(getRow);
+    console.log('i' + i);
+
+    for (let j = 0; j < 16; j++) {
+      const getCol = document.createElement('div');
+      getCol.classList.add('grid', 'col');
+      getRow.appendChild(getCol);
+      console.log('h' + j);
+    }
+  }
+}
+
+createGrid();

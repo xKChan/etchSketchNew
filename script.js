88ed578b260ea6daf3ec5110341fd979289c1066
xkc <xkylechan@gmail.com>
2024-11-15 00:48:27 -0700

    got hover effect to work.  Found out the problem was, I was calling the quaryselector at the top before the creategrid was called so was not finding any classes.. As the classes were created after .. Note to self, pay attention to how things are called and the order they are executed

--- script.js
+++ script.js
@@ -5,15 +5,23 @@ function createGrid() {
     const getRow = document.createElement('div');
     getRow.classList.add('grid', 'row');
     getContainer.appendChild(getRow);
-    console.log('i' + i);
+    // console.log('i' + i);
 
     for (let j = 0; j < 16; j++) {
       const getCol = document.createElement('div');
       getCol.classList.add('grid', 'col');
       getRow.appendChild(getCol);
-      console.log('h' + j);
+      // console.log('h' + j);
     }
   }
 }
 
 createGrid();
+
+const getHoveredSquare = document.querySelectorAll('.col');
+
+getHoveredSquare.forEach(box => {
+  box.addEventListener('mouseover', () => {
+    box.classList.add('hovered');
+  });
+});
